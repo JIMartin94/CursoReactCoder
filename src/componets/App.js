7dd5daff5ac56d6f81517e698f9c03a88ccee6f3
juaniMartin <juani-martin@hotmail.com>
2022-05-30 17:26:18 -0300

    Agregando navBar

---
+++ src/componets/App.js
@@ -0,0 +1,16 @@
+import Header from "./NavBar";
+import Main from "./Main";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+const App = () =>{
+    return(
+        <>
+            <NavBar/> 
+            <Main/>
+            <Footer/>
+        </>
+    ) 
+}
+
+export default App;
